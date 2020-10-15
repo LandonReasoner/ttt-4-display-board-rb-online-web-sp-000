1e48d3f9583ac23d7b5eaa68898b8b05591692fd
Landon Reasoner <reasoner.landon90@gmail.com>
2020-10-15 01:05:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  puts 
-  
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
